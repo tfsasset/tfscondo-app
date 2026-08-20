51a7196b6dc3e72e154976dd2ffe9177c987e34c
warisara samaksaman <mywaaari@MacBook-Air-khxng-warisara.local>
2026-08-20 22:29:04 +0700

    update database config

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 
 // ไปเอา URL และ KEY มาจากเมนู Project Settings > API ในเว็บ Supabase
 const supabaseUrl =  'https://atbyudnixujiwlxepchh.supabase.co'
-const supabaseAnonKey = 'sb_publishable_leBQo88PZWYV800h4C6dUA_Oj4gMzMm'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0Ynl1ZG5peHVqaXdseGVwY2hoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcxNTM0MDQsImV4cCI6MjEwMjcyOTQwNH0.zaZOk2dJzXwip3n9DxBXDObELI5XFSUoEe2NzdfdD1c'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
